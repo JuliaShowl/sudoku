8d09757936ae78cc1304f59475350efcfd1d4fa9
Julia Showl <showl.julia@gmail.com>
2021-01-04 14:03:45 -0500

    Initial commit

---
+++ sudoku_board.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Julia Showl
+ * 
+ * Last modified: 1/4/21
+ * 
+ * This project contains code to generate a sudoku board
+ */
+import javax.swing.*;
+import java.awt.*;
+
+public class sudoku_board {
+    public static void main(String[] args){
+
+    }
+}
---
+++ sudoku_solver.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Julia Showl
+ * 
+ * Last modified: 1/4/21
+ * 
+ * This project contains code to validate and solve sudoku puzzles
+ */
+public class sudoku_solver{
+
+    /**
+     * This function varifies that a sudoku board is valid
+     * @param board the board to be checked
+     * @return true if the board is valid
+     *         false otherwise
+     */
+    public boolean isValid(int[][] board){
+        return true;
+    }
+
+    /**
+     * This function solves the sudoku board
+     * @param board the board to be solved
+     * @return the solved board
+     */
+    public int[][] solve(int[][] board){
+        return board;
+    }
+}
